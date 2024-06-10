4a55d879d70d222218df8406dff9fc739c75700f
Ishika Santhalia <ishikacsp@gmail.com>
2024-06-10 18:07:02 +0530

    commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+}
